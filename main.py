30d11e4711e94ad1d8185badc775bb1233586c2c
Eric <eric.elmoznino@gmail.com>
2017-06-30 17:24:33 -0400

    Modified a file path

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import Attitude_1
 import Helpers
 
 att_model = Attitude_1.Model(Helpers.Configuration(epochs=20), 100, 100)
-att_model.train('/Users/Eric/ML_data/train_data',
-                '/Users/Eric/ML_data/validation_data',
-                '/Users/Eric/ML_data/test_data')
+att_model.train('/Users/Eric/ML_data/Attitude_1/train_data',
+                '/Users/Eric/ML_data/Attitude_1/validation_data',
+                '/Users/Eric/ML_data/Attitude_1/test_data')
 predictions = att_model.predict('/Users/Eric/ML_data/prediction_data')
 print(predictions)
