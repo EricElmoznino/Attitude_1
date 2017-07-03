831b8167ed466df7088e0815b319d35a62f8ed31
Eric <eric.elmoznino@gmail.com>
2017-07-03 14:01:15 -0400

    Single angle tests done. Going back to predicting all axes of rotation.

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ att_model = Attitude_1.Model(Helpers.Configuration(epochs=30, batch_size=10, dro
 #                 '/Users/Eric/ML_data/Attitude_1/data_pitch/validation_data',
 #                 '/Users/Eric/ML_data/Attitude_1/data_pitch/test_data')
 # predictions = att_model.predict('/Users/Eric/ML_data/Attitude_1/data_pitch/prediction_data')
-att_model.train('./data_yawpitch/train_data',
-                './data_yawpitch/validation_data',
-                './data_yawpitch/test_data')
-predictions = att_model.predict('./data_yawpitch/prediction_data')
+att_model.train('./data/train_data',
+                './data/validation_data',
+                './data/test_data')
+predictions = att_model.predict('./data/prediction_data')
 print(predictions)
