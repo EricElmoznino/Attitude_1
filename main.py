8542f287a0ace66e85107b2b3a945b4d1d38c25e
Eric <eric.elmoznino@gmail.com>
2017-07-02 23:58:50 -0400

    Experimenting with only single angles at a time

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import Attitude_1
 import Helpers
 
 att_model = Attitude_1.Model(Helpers.Configuration(epochs=30, batch_size=10), 100, 100)
-# att_model.train('/Users/Eric/ML_data/Attitude_1/data_roll/train_data',
-#                 '/Users/Eric/ML_data/Attitude_1/data_roll/validation_data',
-#                 '/Users/Eric/ML_data/Attitude_1/data_roll/test_data')
-# predictions = att_model.predict('/Users/Eric/ML_data/Attitude_1/data_roll/prediction_data')
-att_model.train('./data_roll/train_data',
-                './data_roll/validation_data',
-                './data_roll/test_data')
-predictions = att_model.predict('./data_roll/prediction_data')
+# att_model.train('/Users/Eric/ML_data/Attitude_1/data_pitch/train_data',
+#                 '/Users/Eric/ML_data/Attitude_1/data_pitch/validation_data',
+#                 '/Users/Eric/ML_data/Attitude_1/data_pitch/test_data')
+# predictions = att_model.predict('/Users/Eric/ML_data/Attitude_1/data_pitch/prediction_data')
+att_model.train('./data_pitch/train_data',
+                './data_pitch/validation_data',
+                './data_pitch/test_data')
+predictions = att_model.predict('./data_pitch/prediction_data')
 print(predictions)
